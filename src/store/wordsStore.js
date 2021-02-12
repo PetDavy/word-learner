06f76161663514ecfad2917915435b6a2d194199
Petro Davybida <petdavy1991@gmail.com>
2021-02-12 15:53:30 +0200

    split main sate into modules

--- src/store/wordsStore.js
+++ src/store/wordsStore.js
@@ -4,16 +4,11 @@ const baseModes = {
   inWordsShowMode: false,
 };
 
-const CARDS_AMOUNT = 1;
-
 export default {
   state: {
     modes: baseModes,
     activeWord: { word: 'No Word', meanings: [] },
     savedWords: [],
-    shownCards: [],
-    availableWords: [],
-    currentCards: [],
   },
   mutations: {
     changeMode(state, payload) {
@@ -31,43 +26,19 @@ export default {
     addWord(state, payload) {
       state.savedWords = [...state.savedWords, payload.newWord];
     },
-    deleteWord(state, payload) {
-      if (state.currentCards.includes(state.savedWords[payload.wordIndex])) {
-        state.currentCards = [];
+  },
+  actions: {
+    deleteWord({ commit, state, rootState }, payload) {
+      if (rootState.gameStore.currentCards.includes(state.savedWords[payload.wordIndex])) {
+        commit('clearCurrentCards', null, { root: true });
       }
 
       state.savedWords = state.savedWords.filter((word, index) => index !== payload.wordIndex);
     },
-    changeCurrentCards(state) {
-      state.availableWords = state.savedWords.filter((word) => (
-        !state.shownCards.includes(word)
-      ));
-
-      if (state.currentCards.length < CARDS_AMOUNT && state.savedWords.length) {
-        // state.availableWords = [...state.availableWords, ...state.currentCards];
-
-        state.currentCards = state.availableWords.sort(() => (
-          Math.random() - 0.5
-        )).splice(0, CARDS_AMOUNT);
-
-        state.shownCards = [...state.shownCards, ...state.currentCards];
-      }
-    },
-    clearCurrentCards(state) {
-      state.currentCards = [];
-    },
   },
   getters: {
-    wordsCount(state) {
-      return state.currentCards.length || state.savedWords.length > state.shownCards.length;
+    wordsCount(state, getters) {
+      return getters.currentCards.length || state.savedWords.length > getters.shownCards.length;
     },
-    currentCards(state) {
-      return state.currentCards;
-    },
-    availableWords(state) {
-      return state.availableWords.sort(() => Math.random() - 0.5);
-    },
-  },
-  actions: {
   },
 };
